488b332e69dbf6fdf8d5d45c488fa0dee87bcc8b
AlejoART <cloud91art@gmail.com>
2019-08-21 15:26:42 -0700

    Pushing from windows

--- src/test/java/com/training/booking2/OptionsPage.java
+++ src/test/java/com/training/booking2/OptionsPage.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class OptionsPage extends BasePage {
     By lowestPrice = By.partialLinkText("Price (lowest first)");
     By availability = By.partialLinkText("See availability");
-    //WebElement reserveBtn = driver.findElements(By.cssSelector("span[class='b-button__text']")).get(1);
+    By reserveBtn = By.xpath("//*[@id=\"group_recommendation\"]/table/tbody/tr/td[2]/table/tbody/tr[2]/td/a/span");
 //    By numRooms = By.xpath("//*[@id=\"hprt_nos_select_534593501_189325125_4_1_0\"]");
 //    By reserveBtn2 = By.cssSelector("#b_tt_holder_1");
     By hotelName = By.xpath("//*[@id=\"hp_hotel_name\"]");//me dice q q esta undefined
@@ -29,7 +29,9 @@ public class OptionsPage extends BasePage {
         /*WebDriverWait ewait2 = new WebDriverWait(driver,10);
         ewait2.until(ExpectedConditions.titleContains("Chile - Booking.com"));
 */
-        driver.findElements(By.cssSelector("span[class='b-button__text']")).get(1).click();
+
+//        driver.findElements(By.cssSelector("span[class='b-button__text']")).get(1).click();
+        driver.findElement(reserveBtn).click();
         //Thread.sleep(5000);
 
 //        if(Integer.parseInt(driver.findElement(numRooms).getText())==0){
